6a5766af8597c30b5f0309ae9ce6471ba845fcc3
JORGE CASCANTE <cascante.george@gmail.com>
2025-08-28 10:51:07 -0600

    Fixes indentation

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -18,27 +18,27 @@ const hersoCommonTemplate = (fields: Object) => {
 };
 
 export const POST: APIRoute = async ({ request }) => {
-    try {
-      const json = await request.json();
-      const template = hersoCommonTemplate(json);
-        const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: import.meta.env.HERSO_SENDER_EMAIL,
-            pass: import.meta.env.HERSO_SENDER_PASSWORD,
-        },
-        });
+  try {
+    const json = await request.json();
+    const template = hersoCommonTemplate(json);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: import.meta.env.HERSO_SENDER_EMAIL,
+        pass: import.meta.env.HERSO_SENDER_PASSWORD,
+      },
+    });
 
-        await transporter.sendMail({
-        from: import.meta.env.HERSO_SENDER_EMAIL,
-        to: import.meta.env.HERSO_TO_EMAIL,
-        subject: `Contacto HERSO page`,
-        html: template
-        });
+    await transporter.sendMail({
+      from: import.meta.env.HERSO_SENDER_EMAIL,
+      to: import.meta.env.HERSO_TO_EMAIL,
+      subject: `Contacto HERSO page`,
+      html: template
+    });
 
-        return new Response(JSON.stringify({ success: true }), { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return new Response(JSON.stringify({ success: false, error }), { status: 500 });
-    }
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ success: false, error }), { status: 500 });
+  }
 };
